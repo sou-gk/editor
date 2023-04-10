ed251d1734d16fea63e89f32fdba0c9fb1ba9608
sou-gk <123944173+sou-gk@users.noreply.github.com>
2023-04-10 21:59:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 // main.js
-var input = document.getElementById('input');
-var output = document.getElementById('output');
+let input = document.getElementById('input');
+let output = document.getElementById('output');
 
 // プレビュー表示の更新
 function preview() {
-  var html = input.value;
-  var parser = new DOMParser();
-  var doc = parser.parseFromString(html, 'text/html');
+  let html = input.value;
+  let parser = new DOMParser();
+  ;et doc = parser.parseFromString(html, 'text/html');
   output.innerHTML = doc.documentElement.textContent;
 }
 
 // ローカルストレージに保存
 function save() {
-  var html = input.value;
+  let html = input.value;
   localStorage.setItem('html', html);
 }
 
 // 初期表示
 (function init() {
-  var html = localStorage.getItem('html');
+  let html = localStorage.getItem('html');
   if (html) {
     input.value = html;
     preview();
